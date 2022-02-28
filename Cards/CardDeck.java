cdecfe3613ad6894b4433477d63c6b9179579a31
Vivek Dalvi <vivekdalvi@hotmail.com>
2022-02-27 19:02:08 -0800

    added comment header

--- Cards/CardDeck.java
+++ Cards/CardDeck.java
@@ -12,49 +12,49 @@ public class CardDeck {
    private boolean[] cardsDealt;
    private int numberOfCardsDrawn;
    private Random gen;
-   
+
    // Class constants
    private static final boolean CARD_DRAWN = true;
    private static final boolean CARD_NOT_DRAWN = false;
    private static final int DECK_SIZE = 52;
-   
+
    // Constructor
-   public CardDeck(){
+   public CardDeck() {
       cardsDealt = new boolean[DECK_SIZE];
       numberOfCardsDrawn = 0;
       gen = new Random();
    }
-   
+
    // Methods
-   public Card drawACard(){
-      if(numberOfCardsDrawn >= DECK_SIZE){
-         return null;  // no cards available.
+   public Card drawACard() {
+      if (numberOfCardsDrawn >= DECK_SIZE) {
+         return null; // no cards available.
       }
       int nextCardIndex;
-      do{
+      do {
          nextCardIndex = gen.nextInt(DECK_SIZE);
       } while (cardsDealt[nextCardIndex]);
       cardsDealt[nextCardIndex] = CARD_DRAWN;
       numberOfCardsDrawn++;
       return new Card(nextCardIndex);
    }
-   
-   public int getDeckSize(){
+
+   public int getDeckSize() {
       return DECK_SIZE;
    }
-   
-   public int getNumberOfCardsDrawn(){
+
+   public int getNumberOfCardsDrawn() {
       return numberOfCardsDrawn;
    }
-   
-   public void resetDeck(){
+
+   public void resetDeck() {
       Arrays.fill(cardsDealt, CARD_NOT_DRAWN);
       numberOfCardsDrawn = 0;
    }
-   
-   public String toString(){
+
+   public String toString() {
       String str = "Deck of " + DECK_SIZE + " cards of which " +
-              numberOfCardsDrawn + " have been drans.";
+            numberOfCardsDrawn + " have been drans.";
       return str;
    }
 }
