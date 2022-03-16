30d1a7a6bf1d6306591892e4cbad9fe882257681
Vivek Dalvi <vivekdalvi@hotmail.com>
2022-03-15 20:44:33 -0700

    inheritence

---
+++ foobar/test.java
@@ -0,0 +1,13 @@
+package foobar;
+
+public class test {
+    public static void main(String[] args) {
+        Foo[] pity = { new Baz(), new Bar(), new Mumble(), new Foo() };
+        for (int i = 0; i < pity.length; i++) {
+            System.out.println(pity[i]);
+            pity[i].method1();
+            pity[i].method2();
+            System.out.println();
+        }
+    }
+}
