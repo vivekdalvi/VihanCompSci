a32819cee75639bc287f20b6f81ee805d2d7298b
Vivek Dalvi <vivekdalvi@hotmail.com>
2022-05-10 21:17:43 -0700

    recent changes

---
+++ GradStudent.java
@@ -0,0 +1,15 @@
+public class GradStudent extends Student {
+    public String getFood() {
+        return "Taco";
+    }
+
+    public String getInfo() {
+        // return super.getInfo();
+        return "Eating";
+    }
+
+    // public GradStudent(int test) {
+
+    // }
+
+}
---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public String getFood() {
+        return "Pizza";
+    }
+
+    public String getInfo() {
+        return "Studying";
+    }
+
+}
